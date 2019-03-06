692bb32bb9eab38e158ef81f7241c68a2d2774f5
Matheus Fernandes <matheusfernandesluz@gmail.com>
2019-03-06 18:00:33 -0400

    commit 12

--- unitprincipal.pas
+++ unitprincipal.pas
@@ -17,11 +17,12 @@ type
     btSelecionar: TButton;
     btSalvar: TButton;
     Edit1: TEdit;
+    Edit2: TEdit;
     Label1: TLabel;
+    Label2: TLabel;
     procedure btCarregarClick(Sender: TObject);
     procedure btSalvarClick(Sender: TObject);
     procedure btSelecionarClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -39,89 +40,107 @@ implementation
 {$R *.lfm}
 
 procedure SalvarArquivoDestino(local: String);
+var
+  data, hora: String;
 begin
+
+  data:= StringReplace(DateToStr(Date()), '/', '.', [rfReplaceAll]);
+  hora:= StringReplace(TimeToStr(Time()), ':', '.', [rfReplaceAll]);
+
   try
-    try
-      arquivoDestino.SaveToFile(local+'\extrato.html');
-      ShowMessage('Salvo com sucesso');
-    except
-      ShowMessage('Não foi possível salvar o arquivo.');
-    end;
+   arquivoDestino.SaveToFile(local+'\EXTRATO - ' + data  + ' - ' + hora + '.html');
+   ShowMessage('Arquivo Salvo');
   finally
-    arquivoDestino.Free;
+   arquivoDestino.Free;
+   cont:=0;
   end;
 end;
 
 procedure CriaTabela();
 begin
- arquivoDestino.add('<!DOCTYPE html>');
- arquivoDestino.add('<html>');
- arquivoDestino.add('<head>');
- arquivoDestino.add('<meta charset="utf-8">');
- arquivoDestino.add('<title> Extrato bancário </title>');
- arquivoDestino.add('</head>');
- arquivoDestino.add('<body>');
- arquivoDestino.add('<h2> EXTRATO BANCÁRIO </h2>');
- arquivoDestino.add('<table>');
- arquivoDestino.add('<thead style="color: white; background-color: black;">');
- arquivoDestino.add('<tr>');
- arquivoDestino.add('<th style="width: 100px; text-align: center;"> Data </th>');
- arquivoDestino.add('<th style="width: 100px; text-align: center;"> Documento </th>');
- arquivoDestino.add('<th style="width: 600px; text-align: center;"> Histórico </th>');
- arquivoDestino.add('<th style="width: 100px; text-align: center;"> Valor </th>');
- arquivoDestino.add('<th style="width: 100px; text-align: center;"> Saldo </th>');
- arquivoDestino.add('</tr>');
- arquivoDestino.add('</thead>');
- arquivoDestino.add('<tbody>');
+  arquivoDestino:= TStringList.Create;
+
+  arquivoDestino.add('<!DOCTYPE html>');
+  arquivoDestino.add('<html>');
+  arquivoDestino.add('<head>');
+  arquivoDestino.add('<meta charset="utf-8">');
+  arquivoDestino.add('<title> Extrato bancário </title>');
+  arquivoDestino.add('</head>');
+  arquivoDestino.add('<body>');
+  arquivoDestino.add('<h2> EXTRATO BANCÁRIO </h2>');
+  arquivoDestino.add('<table>');
+  arquivoDestino.add('<thead style="color: white; background-color: black;">');
+  arquivoDestino.add('<tr>');
+  arquivoDestino.add('<th style="width: 100px; text-align: center;"> Data </th>');
+  arquivoDestino.add('<th style="width: 100px; text-align: center;"> Documento </th>');
+  arquivoDestino.add('<th style="width: 600px; text-align: center;"> Histórico </th>');
+  arquivoDestino.add('<th style="width: 100px; text-align: center;"> Valor </th>');
+  arquivoDestino.add('<th style="width: 100px; text-align: center;"> Saldo </th>');
+  arquivoDestino.add('</tr>');
+  arquivoDestino.add('</thead>');
+  arquivoDestino.add('<tbody>');
+
+end;
+
+procedure IncrementaLinhaExtrato(data, documento, historico, valor, saldo, color: String);
+begin
+  arquivoDestino.Add('<tr>');
+  arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + data + '</td>');
+  arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + documento + '</td>');
+  arquivoDestino.Add('<td style="border: solid 1px black; text-align: left;">' + historico + '</td>');
+  arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + valor + '</td>');
+  arquivoDestino.Add('<td style="border: solid 1px black; text-align: center; background-color:' + color + '"><b>' + saldo + '</b></td>');
+  arquivoDestino.Add('</tr>');
+  Inc(cont);
+end;
+
+function ModificaValor(numero: String): String;
+var
+  separadorDec: Char;
+  aux: Double;
+begin
+
+  if (numero = '') then Exit;
+  try
+    separadorDec:= FormatSettings.DecimalSeparator;
+    FormatSettings.DecimalSeparator:='.';
+    aux:= StrToFloat(numero);
+    FormatSettings.DecimalSeparator:=',';
+    ModificaValor:= FormatFloat('#,##0.00', aux);
+  finally
+    FormatSettings.DecimalSeparator:= separadorDec;
+  end;
 
 end;
 
 procedure ValidarDados(campos: array of String);
 var
- Data:TDateTime;
+ Date: TDateTime;
  valida: Boolean;
- valor, saldo: Double;
- separadorDec, color: String;
+ data, documento, historico, valor, saldo, color: String;
 begin
   valida:= false;
-  if TryStrToDate(campos[0], Data, 'yy-mm-dd', '/') then
+
+  if TryStrToDate(campos[0], Date, 'yy-mm-dd', '/') then
   begin
-   try
-     separadorDec:=DecimalSeparator;
-     DecimalSeparator:=',';
-     valor: StrToFloat(campos[3]);
-     saldo: StrToFloat(campos[4]);
 
-     if valor < 0 then
-     begin
-      color:=clRed;
-     end;
+    data:= campos[0];
+    documento:= campos[1];
+    historico:= campos[2];
+    valor:= ModificaValor(campos[3]);
+    saldo:= ModificaValor(campos[4]);
 
-     campos[3]:= FormatFloat('#.##0.00', valor);
-     campos[4]:= FormatFloat('#.##0.00', saldo);
-   finally
-     DecimalSeparator:=separadorDec;
-   end;
+    if saldo.Contains('-') then color:='red'
+    else color:='lightgray';
 
-   if (campos[1] = '') then
-   begin
-    if (campos[2] = 'SALDO') or (campos[2] = 'SALDO ANTERIOR') then
-     valida:=true;
-   end
-   else valida:=true;
+    if (documento = '') then
+    begin
+     if (historico = 'SALDO') or (historico = 'SALDO ANTERIOR') then valida:=true;
+    end
+    else valida:=true;
   end;
 
-  if valida = true then
-  begin
-    Inc(cont);
-    arquivoDestino.Add('<tr>');
-    arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + campos[0] + '</td>');
-    arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + campos[1] + '</td>');
-    arquivoDestino.Add('<td style="border: solid 1px black; text-align: left;">' + campos[2] + '</td>');
-    arquivoDestino.Add('<td style="border: solid 1px black; text-align: center;">' + campos[3] + '</td>');
-    arquivoDestino.Add('<td style="border: solid 1px black; text-align: center; background-color: lightgray;"><b>' + campos[4] + '</b></td>');
-    arquivoDestino.Add('</tr>');
-  end;
+  if valida = true then IncrementaLinhaExtrato(data, documento, historico, valor, saldo, color);
 
 end;
 
@@ -148,16 +167,17 @@ begin
 
   try
     arquivoOrigem.LoadFromFile(nomeArquivo);
-    CriaTabela();
+    if arquivoDestino = nil then CriaTabela();
     i:=0;
-    while i <= arquivoOrigem.Count-1 do
+    while i < arquivoOrigem.Count-1 do
     begin
       SeparaCampos(arquivoOrigem[i]);
       Inc(i);
     end;
-    if cont < 1 then
+    if (i = 0) and (cont < 1) then
     begin
      ShowMessage('Arquivo de entrada inválido.');
+     arquivoOrigem.Free;
      Exit;
     end;
     Principal.btSalvar.Enabled:=true;
@@ -178,6 +198,7 @@ begin
     open:= TOpenDialog.Create(nil);
     open.Filter:='Arquivo CSV | *.csv';
     open.Execute;
+    Edit2.Text:= open.FileName;
     nomeArquivo:= open.FileName;
   finally
     open.Free;
@@ -196,20 +217,15 @@ end;
 
 procedure TPrincipal.btSelecionarClick(Sender: TObject);
 var
-  open: TOpenDialog;
+  open: TSelectDirectoryDialog;
 begin
   try
-    open:= TOpenDialog.Create(nil);
+    open:= TSelectDirectoryDialog.Create(nil);
     open.Execute;
     Edit1.Text:= open.FileName;
   finally
     open.Free;
   end;
-end;
-
-procedure TPrincipal.FormCreate(Sender: TObject);
-begin
-  arquivoDestino:= TStringList.Create;
 end;
 
 
